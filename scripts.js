dbf8f1f68db2a3701d5687523f8c6585dcc32d23
MeenakshiMcNamara <72270837+MeenakshiMcNamara@users.noreply.github.com>
2025-09-10 21:03:20 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,9 @@
   function tog(id) { ... }
   function togs(c) { ... }
 </script>
+
+fetch('header.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('navbar').innerHTML = html;
+  });
